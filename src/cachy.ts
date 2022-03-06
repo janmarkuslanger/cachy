1ce3b8106dd418dae4b002f5495b8c78a5597d1a
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2022-03-06 18:55:23 +0000

    Use Message in cachy

--- src/cachy.ts
+++ src/cachy.ts
@@ -1,11 +1,12 @@
 import Client from './client';
+import Message from './messages';
 
 class Cachy {
     private client: Client;
 
     constructor(client: Client) {
         if (!client) {
-            throw new Error('No client given in cachy');
+            throw new Error(Message.NO_CLIENT_GIVEN);
         }
 
         this.client = client;
