c10b9433cf10a315404535c7db83e3f13bfeaf7d
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2022-03-06 19:31:32 +0000

    Add DemoApiClient

---
+++ test/demo-api-client.ts
@@ -0,0 +1,21 @@
+import { Client } from '../src';
+
+const DemoApiClient: Client = {
+    get(url, params) {
+
+        if (url === 'demo') {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve({ data: 1 });
+                }, 1000);
+            })
+        }
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({ data: 1 });
+            }, 1000);
+        }) 
+
+    }
+}
