02d6f6d1dec272725c6f36dec70dd085a50a7570
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2022-03-06 19:40:52 +0000

    Change parameters param to optional

--- src/client.ts
+++ src/client.ts
@@ -4,10 +4,10 @@ type Parameter = {
 }
 
 interface Client {
-    get?(url: Url, parameter: Parameter): Promise<any>;
-    post?(url: Url, parameter: Parameter): Promise<any>;
-    delete?(url: Url, parameter: Parameter): Promise<any>;
-    update?(url: Url, parameter: Parameter): Promise<any>;
+    get?(url: Url, parameter?: Parameter): Promise<any>;
+    post?(url: Url, parameter?: Parameter): Promise<any>;
+    delete?(url: Url, parameter?: Parameter): Promise<any>;
+    update?(url: Url, parameter?: Parameter): Promise<any>;
 };
 
 export default Client;
